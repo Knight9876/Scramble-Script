ea3c4066bc588f18fb1cb1fc076dadd03d71f078
Yash Kamble <yashkamble1234567890@gmail.com>
2025-01-30 05:42:07 +0530

    Delete models directory

--- models/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-  username: String,
-  timeTaken: Number, // Store time taken in seconds
-});
-
-module.exports = mongoose.model("User", userSchema);
